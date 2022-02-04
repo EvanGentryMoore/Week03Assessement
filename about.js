6cb362699e43e91f5ebbd4bfd2d1401579e2491d
Evan Moore <even.gentry.moore@gmail.com>
2022-02-04 12:36:03 -0700

    Trying this again

---
+++ about.js
@@ -0,0 +1,23 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert("You submitted the form.");
+	// console.log('form submit');
+}
+
+const compliment = (event) =>{
+	event.preventDefault();
+
+	alert("I see you're a person of culture as well.")
+}
+
+
+let form = document.querySelector('form#contact');
+let img = document.querySelector('img')
+
+form.addEventListener('submit', handleSubmit);
+img.addEventListener('mouseover', compliment);
